ee4e318f375fa9a33fd6a9b0784886feb3c8f8e1
printf83 <printf83@gmail.com>
2022-12-28 12:45:13 +0800

    update

--- src/core/base/attachDisabled.ts
+++ src/core/base/attachDisabled.ts
@@ -1,7 +1,7 @@
 import { addIntoClassList } from '../fn/addIntoClassList.js';
 import { IBase, IAttachResult } from './base.interface';
 export const attachDisabled = (key:string, elem:HTMLElement, opt:IBase):IAttachResult => {
-	if (opt[key] === true && key === "disabled") {
+	if (key === "disabled" && opt[key] === true) {
 		elem = addIntoClassList(elem, key);
 		elem.setAttribute(`aria-${key}`, opt[key]);
 		elem.setAttribute(key, key);
