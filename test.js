8b2428362dcf7b75776fc3dc709be693b2f7102b
printf83 <printf83@gmail.com>
2023-01-02 21:52:44 +0800

    update

--- test.js
+++
@@ -1,8 +0,0 @@
-import { core } from "./lib/index.js";
-
-console.log(core.UUID());
-console.log(core.UUID());
-
-let body = document.getElementsByTagName("BODY");
-core.appendChild(body[0], [{ tag: "h1", elem: "hello world" }]);
-
