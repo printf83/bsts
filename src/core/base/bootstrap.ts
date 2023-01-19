49f2210946b4574b17fe408bcc3c0caf691b5555
printf83 <printf83@gmail.com>
2023-01-19 15:29:03 +0800

    update

--- src/core/base/bootstrap.ts
+++ src/core/base/bootstrap.ts
@@ -1,4 +1,4 @@
-// namespace bootstrapLTBase {
+// export namespace bootstrapLTBase {
 // 	type base5 = 0 | 1 | 2 | 3 | 4 | 5;
 // 	type base12 = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 // 	type truefalse = true | false;
