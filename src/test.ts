b22b6f963ba71679efd0876186693f6d14fc253f
printf83 <printf83@gmail.com>
2023-03-06 10:31:47 +0800

    update test

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,5 @@
 // import { IAttrBSButton } from "./bs/button.js";
-import { HLevel } from "./ht/h.js";
+// import { HLevel } from "./ht/h.js";
 import { bs, core, ht } from "./index.js";
 // let loream = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque porta tincidunt arcu eu dignissim. Suspendisse nunc tortor, semper ut commodo nec, bibendum ut quam. Vivamus porttitor egestas luctus. Morbi tincidunt tortor eu lorem gravida imperdiet. Vestibulum tincidunt hendrerit tempus. Morbi varius est ac euismod tristique. Fusce a tortor suscipit, lacinia arcu nec, aliquet tortor. Maecenas eu lorem nec ante faucibus sollicitudin. Nunc consequat ullamcorper congue. Praesent cursus velit euismod turpis ultrices, sit amet venenatis massa pharetra. Nunc maximus pharetra purus in maximus.`;
 // let select_option = [
