744be3a1c2e654af263e57d2d309ea56f29dc12e
printf83 <printf83@gmail.com>
2023-03-28 05:32:20 +0800

    update core

--- src/core/_index.ts
+++ src/core/_index.ts
@@ -1,21 +1,94 @@
-import { init, appendChild, prependChild, replaceChild, replaceWith } from "./builder.js";
-import { documentReady } from "./documentReady.js";
+import { manageClass, addClassIntoElement } from "./addClassIntoElement.js";
+import {
+	bootstrapBase,
+	bootstrapType,
+	IBootstrapAttachRule,
+	bootstrapAttachRule,
+	bootstrapRuleDB,
+	isBootstrapType,
+	bsConsNoElemArg,
+	bsConstArg,
+	bsConstArgTag,
+} from "./bootstrap.js";
+import { init, build, node, html, appendChild, prependChild, replaceChild, replaceWith } from "./builder.js";
+import { camel2Dash } from "./camel2Dash.js";
+import { elemReady, documentReady } from "./documentReady.js";
+import { IDEventDB, HTMLElementWithEventDB, addEvent, deleteEvent } from "./eventManager.js";
+import { keyOfType } from "./keyOfType.js";
 import { mergeAttr } from "./mergeAttr.js";
 import { mergeClass } from "./mergeClass.js";
 import { mergeObject } from "./mergeObject.js";
+import { removeChildElement } from "./removeChildElement.js";
 import { removeElement } from "./removeElement.js";
+import { removeEmptyArray } from "./removeEmptyArray.js";
+import {
+	tag,
+	IAria,
+	IAttr,
+	IBootstrap,
+	IData,
+	IElem,
+	IEvent,
+	IGlobal,
+	IGlobalAria,
+	IStyle,
+	ITag,
+	isAttr,
+	isTag,
+	tagConsArg,
+	tagConsNoElemArg,
+} from "./tag.js";
+
 import { UUID } from "./uuid.js";
 
-export const core = {
-	documentReady,
-	removeElement,
-	UUID,
+export {
+	manageClass,
+	addClassIntoElement,
+	bootstrapBase,
+	bootstrapType,
+	IBootstrapAttachRule,
+	bootstrapAttachRule,
+	bootstrapRuleDB,
+	isBootstrapType,
+	bsConsNoElemArg,
+	bsConstArg,
+	bsConstArgTag,
 	init,
+	build,
+	node,
+	html,
 	appendChild,
 	prependChild,
 	replaceChild,
 	replaceWith,
+	camel2Dash,
+	elemReady,
+	documentReady,
+	IDEventDB,
+	HTMLElementWithEventDB,
+	addEvent,
+	deleteEvent,
+	keyOfType,
 	mergeAttr,
 	mergeClass,
 	mergeObject,
+	removeChildElement,
+	removeElement,
+	removeEmptyArray,
+	tag,
+	IAria,
+	IAttr,
+	IBootstrap,
+	IData,
+	IElem,
+	IEvent,
+	IGlobal,
+	IGlobalAria,
+	IStyle,
+	ITag,
+	isAttr,
+	isTag,
+	tagConsArg,
+	tagConsNoElemArg,
+	UUID,
 };
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { tag } from "./core/tag.js";
-import { core } from "./core/_index.js";
+import * as core from "./core/_index.js";
 import { bootstrap } from "./bootstrap/_index.js";
 import { html } from "./html/_index.js";
+import { tag } from "./core/tag.js";
 
 export { core, tag as t, html as h, bootstrap as b };
