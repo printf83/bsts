f43c490be2dc61c5def2d934507d995f5e9578ee
printf83 <printf83@gmail.com>
2023-03-30 01:40:48 +0800

    update

--- src/bootstrap/nav/header/_index.ts
+++ src/bootstrap/nav/header/_index.ts
@@ -2,4 +2,16 @@ import { item, IAttrBSNavItem } from "./item.js";
 import { link, IAttrBSNavLink } from "./link.js";
 import { button, IAttrBSNavButton } from "./button.js";
 import { container, IAttrBSNavContainer } from "./container.js";
-export { container, item, link, button, IAttrBSNavItem, IAttrBSNavLink, IAttrBSNavButton, IAttrBSNavContainer };
+import { containerNav, IAttrBSNavContainerNav } from "./containerNav.js";
+export {
+	container,
+	containerNav,
+	item,
+	link,
+	button,
+	IAttrBSNavItem,
+	IAttrBSNavLink,
+	IAttrBSNavButton,
+	IAttrBSNavContainer,
+	IAttrBSNavContainerNav,
+};
---
+++ src/bootstrap/nav/header/containerNav.ts
@@ -0,0 +1,44 @@
+import { IAttr, IElem } from "../../../core/tag.js";
+import { bsConstArg } from "../../../core/bootstrap.js";
+import { mergeObject } from "../../../core/mergeObject.js";
+import { nav } from "../../../html/nav.js";
+
+export interface IAttrBSNavContainerNav extends IAttr {
+	type?: "tab" | "pill";
+	itemWidth?: "fill" | "justified";
+	vertical?: true;
+	role?: "tablist";
+}
+
+const convert = (attr: IAttrBSNavContainerNav) => {
+	attr = mergeObject(
+		{
+			class: [
+				"nav",
+				attr.type ? `nav-${attr.type}s` : undefined,
+				attr.itemWidth ? `nav-${attr.itemWidth}` : undefined,
+				attr.vertical ? "flex-column" : undefined,
+			],
+			aria: {
+				orientation: attr.vertical ? "vertical" : undefined,
+			},
+		},
+		attr
+	);
+
+	delete attr.type;
+	delete attr.itemWidth;
+	delete attr.vertical;
+
+	return attr;
+};
+
+export class containerNav extends nav {
+	constructor(); //#1
+	constructor(attr: IAttrBSNavContainerNav); //#2
+	constructor(elem: IElem); //#3
+	constructor(attr: IAttrBSNavContainerNav, elem: IElem); //#4
+	constructor(...arg: any[]) {
+		super(bsConstArg<IAttrBSNavContainerNav>("elem", convert, arg));
+	}
+}
