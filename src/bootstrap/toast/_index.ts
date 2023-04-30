8f27296a229ea587553bc807a3d84719793eddeb
printf83 <printf83@gmail.com>
2023-04-30 14:01:13 +0800

    expose toast method

--- src/bootstrap/toast/_index.ts
+++ src/bootstrap/toast/_index.ts
@@ -5,7 +5,7 @@ import { Header, header } from "./header.js";
 import { item, IBsToastItem, Item } from "./item.js";
 import { Time, time } from "./time.js";
 import { Title, title } from "./title.js";
-import { IBsToastSimple, show, Simple } from "./_fn.js";
+import { dispose, getInstance, getOrCreateInstance, hide, IBsToastSimple, isShown, show, Simple } from "./_fn.js";
 
 export {
 	item,
@@ -20,6 +20,11 @@ export {
 	IBsToastItem,
 	IBsToastSimple as IBsToastTemplate,
 	show,
+	getInstance,
+	getOrCreateInstance,
+	hide,
+	dispose,
+	isShown,
 };
 
 export { Item, Body, Btnclose, Container, Header, Time, Title, Simple };
