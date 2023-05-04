9c25bc5800f7673653b070d584387e8b46b79fb7
printf83 <printf83@gmail.com>
2023-05-04 20:31:12 +0800

    update rndBetween function

--- src/core/rndBetween.ts
+++ src/core/rndBetween.ts
@@ -1,19 +1,37 @@
-let lastRndBeetween = 0;
+let lastRndBetween: number;
 
 export const rndBetween = (min: number, max: number) => {
 	if (min === max) {
 		return min;
-	} else {
-		if (max < min) {
-			max = min + 1;
-		}
-
-		let result = Math.floor(Math.random() * (max - min + 1) + min);
-		if (result !== lastRndBeetween) {
-			lastRndBeetween = result;
-			return result;
-		} else {
-			return rndBetween(min, max);
-		}
 	}
+
+	if (max < min) {
+		max = min + 1;
+	}
+
+	let result: number;
+	do {
+		result = Math.floor(Math.random() * (max - min + 1) + min);
+	} while (result === lastRndBetween);
+
+	lastRndBetween = result;
+	return result;
 };
+
+// export const rndBetween = (min: number, max: number): number => {
+// 	if (min === max) {
+// 		return min;
+// 	} else {
+// 		if (max < min) {
+// 			max = min + 1;
+// 		}
+
+// 		let result = Math.floor(Math.random() * (max - min + 1) + min);
+// 		if (result !== lastRndBeetween) {
+// 			lastRndBeetween = result;
+// 			return result;
+// 		} else {
+// 			return rndBetween(min, max);
+// 		}
+// 	}
+// };
