39e26ea6eeb0d468b8bb4e27faf9b1cbc2b2288d
printf83 <printf83@gmail.com>
2023-06-01 18:57:38 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import * as b from "./bootstrap/_index.js";
 import * as h from "./html/_index.js";
 import { tag as t, strHtml as s } from "./core/tag.js";
 import * as I from "./___index.js";
+import * as $ from "./fn.js";
 
-export { core, t, s, h, b, I };
+export { core, t, s, h, b, I, $ };
