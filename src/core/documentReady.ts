6fb55ab75375a0723b20c90a3f3321938bb79b29
printf83 <printf83@gmail.com>
2023-06-15 10:15:14 +0800

    fix MutationObserver using so much memory

--- src/core/documentReady.ts
+++ src/core/documentReady.ts
@@ -8,6 +8,7 @@ import { dropdownMenuStyle } from "./css/dropdownMenuStyle.js";
 import { svgInLinkAndButton } from "./css/svgInLinkAndButton.js";
 import { tableResponsive } from "./css/tableResponsive.js";
 import { bstsConsole as console } from "./console.js";
+import { UUID } from "./uuid.js";
 
 //set css on document ready
 const setCSS = () => {
@@ -27,23 +28,32 @@ const setCSS = () => {
 };
 
 //setup DOMInserted
+var BSTS_DOMWatcherID: string = ""; //GLOBAL
 const setupDOMWatcher = () => {
 	const observer = new MutationObserver(function (m) {
-		//i dont know if this is good or not
-		m.forEach(function (e) {
-			if (e.addedNodes && e.addedNodes.length > 0) {
-				const elem = document.documentElement.querySelectorAll(".bs-build-event");
-				if (elem && elem.length > 0) {
-					elem.forEach((i) => {
-						i.classList.remove("bs-build-event");
-						i.dispatchEvent(new CustomEvent("build"));
-					});
-				}
-			}
-		});
+		if (m[0].addedNodes && m[0].addedNodes.length > 0) {
+			BSTS_DOMWatcherID = UUID();
+			setTimeout(
+				(id: string) => {
+					if (id === BSTS_DOMWatcherID) {
+						const elem = document.querySelectorAll(".bs-build-event");
+						if (elem && elem.length > 0) {
+							elem.forEach((i) => {
+								i.classList.remove("bs-build-event");
+								i.dispatchEvent(new CustomEvent("build"));
+							});
+						}
+					} else {
+						console.log("DOMWatcherID change");
+					}
+				},
+				0,
+				BSTS_DOMWatcherID
+			);
+		}
 	});
 
-	observer.observe(document.documentElement, {
+	observer.observe(document, {
 		childList: true,
 		subtree: true,
 	});
