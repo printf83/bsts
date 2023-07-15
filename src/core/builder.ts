c70cf7df0010da7f1abafb2446d6d683977cf646
printf83 <printf83@gmail.com>
2023-07-15 10:33:45 +0800

    remove escape backquote. using hilightjs now

--- src/core/builder.ts
+++ src/core/builder.ts
@@ -33,8 +33,8 @@ const processElem = (i: number | string | tag | strHtml, e: tag, element: Elemen
 
 			if (e.tag === "pre" || e.tag === "code") {
 				// element.insertAdjacentText("beforeend", g.replace(/\`/g, "\\`").replace(/\$\{/g, "\\$\\{"));
-				// element.insertAdjacentText("beforeend", g);
-				element.insertAdjacentText("beforeend", g.replaceAll("`", '"'));
+				element.insertAdjacentText("beforeend", g);
+				// element.insertAdjacentText("beforeend", g.replaceAll("`", '"'));
 			} else {
 				let m = markup(g);
 				if (typeof m === "string") {
