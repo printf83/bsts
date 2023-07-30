b7d23fb7761851833ad3753c830bb00d57c6adf1
printf83 <printf83@gmail.com>
2023-07-30 23:20:28 +0800

    will update CSSVar function

--- src/core/CSSVar.ts
+++ src/core/CSSVar.ts
@@ -1,5 +1,37 @@
 import { bstsConsole as console } from "./console.js";
 
+export const hexToHSL = (hex?: string, alpha?: number) => {
+	const rgb = hexToRGB(hex, alpha);
+	if (rgb) {
+		(rgb.r /= 255), (rgb.g /= 255), (rgb.b /= 255);
+		var max = Math.max(rgb.r, rgb.g, rgb.b),
+			min = Math.min(rgb.r, rgb.g, rgb.b);
+		var h,
+			s,
+			l = (max + min) / 2;
+		if (max == min) {
+			h = s = 0; // achromatic
+		} else {
+			var d = max - min;
+			s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+			switch (max) {
+				case rgb.r:
+					h = (rgb.g - rgb.b) / d + (rgb.g < rgb.b ? 6 : 0);
+					break;
+				case rgb.g:
+					h = (rgb.b - rgb.r) / d + 2;
+					break;
+				case rgb.b:
+					h = (rgb.r - rgb.g) / d + 4;
+					break;
+			}
+			h! /= 6;
+		}
+	} else {
+		return undefined;
+	}
+};
+
 export const hexToRGB = (hex?: string, alpha?: number) => {
 	if (hex) {
 		var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
@@ -25,6 +57,47 @@ export const hexToRGB = (hex?: string, alpha?: number) => {
 		return undefined;
 	}
 };
+
+// const hue2rgb = (p: number, q: number, t: number) => {
+// 	if (t < 0) t += 1;
+// 	if (t > 1) t -= 1;
+// 	if (t < 1 / 6) return p + (q - p) * 6 * t;
+// 	if (t < 1 / 2) return q;
+// 	if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+// 	return p;
+// };
+
+// const toHex = (x: number) => {
+// 	const hex = Math.round(x * 255).toString(16);
+// 	return hex.length === 1 ? "0" + hex : hex;
+// };
+
+// export const HSLToHex = (hsl?: string) => {
+// 	if (hsl) {
+// 		let v = hsl.replace(/^hsla?\(|\s+|\)$/g, "").split(",");
+// 		let h: number = 0;
+// 		let s: number = 0;
+// 		let l: number = 0;
+
+// 		h /= 360;
+// 		s /= 100;
+// 		l /= 100;
+// 		let r, g, b;
+// 		if (s === 0) {
+// 			r = g = b = l; // achromatic
+// 		} else {
+// 			const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+// 			const p = 2 * l - q;
+// 			r = hue2rgb(p, q, h + 1 / 3);
+// 			g = hue2rgb(p, q, h);
+// 			b = hue2rgb(p, q, h - 1 / 3);
+// 		}
+
+// 		return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
+// 	} else {
+// 		return undefined;
+// 	}
+// };
 
 export const RGBToHex = (rgb?: string) => {
 	if (rgb) {
