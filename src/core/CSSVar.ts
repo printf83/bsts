0373718e7440ef340daf67c0e64ab998ea7bfc72
Hamzah Aziezs <printf83@gmail.com>
2023-08-09 09:32:15 +0800

    update

--- src/core/CSSVar.ts
+++ src/core/CSSVar.ts
@@ -98,47 +98,6 @@ export const hexToRGB = (hex?: string, alpha?: number) => {
 	}
 };
 
-// const hue2rgb = (p: number, q: number, t: number) => {
-// 	if (t < 0) t += 1;
-// 	if (t > 1) t -= 1;
-// 	if (t < 1 / 6) return p + (q - p) * 6 * t;
-// 	if (t < 1 / 2) return q;
-// 	if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
-// 	return p;
-// };
-
-// const toHex = (x: number) => {
-// 	const hex = Math.round(x * 255).toString(16);
-// 	return hex.length === 1 ? "0" + hex : hex;
-// };
-
-// export const HSLToHex = (hsl?: string) => {
-// 	if (hsl) {
-// 		let v = hsl.replace(/^hsla?\(|\s+|\)$/g, "").split(",");
-// 		let h: number = 0;
-// 		let s: number = 0;
-// 		let l: number = 0;
-
-// 		h /= 360;
-// 		s /= 100;
-// 		l /= 100;
-// 		let r, g, b;
-// 		if (s === 0) {
-// 			r = g = b = l; // achromatic
-// 		} else {
-// 			const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-// 			const p = 2 * l - q;
-// 			r = hue2rgb(p, q, h + 1 / 3);
-// 			g = hue2rgb(p, q, h);
-// 			b = hue2rgb(p, q, h - 1 / 3);
-// 		}
-
-// 		return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
-// 	} else {
-// 		return undefined;
-// 	}
-// };
-
 export const RGBToHex = (rgb?: string) => {
 	if (rgb) {
 		let v = rgb.replace(/^rgba?\(|\s+|\)$/g, "").split(",");
--- src/core/builder.ts
+++ src/core/builder.ts
@@ -32,9 +32,7 @@ const processElem = (i: number | string | tag | strHtml, e: tag, element: Elemen
 			}
 
 			if (e.tag === "pre" || e.tag === "code") {
-				// element.insertAdjacentText("beforeend", g.replace(/\`/g, "\\`").replace(/\$\{/g, "\\$\\{"));
 				element.insertAdjacentText("beforeend", g);
-				// element.insertAdjacentText("beforeend", g.replaceAll("`", '"'));
 			} else {
 				let m = markup(g);
 				if (typeof m === "string") {
