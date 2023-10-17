6d4de2bdde4cc3d1219a2b13379a1417f754a747
Hamzah Aziezs <printf83@gmail.com>
2023-10-17 18:36:34 +0800

    update

--- src/interface/bootstrap/btnclose.ts
+++ src/interface/bootstrap/btnclose.ts
@@ -1,30 +1,5 @@
-import { bsConsNoElemArg } from "../core/bootstrap.js";
-import { mergeClass } from "../core/mergeClass.js";
-
-import { button, Button } from "../html/button.js";
+import { Button } from "../../html/button.js";
 
 export interface Btnclose extends Button {
 	white?: boolean;
-}
-
-const convert = (attr: Btnclose) => {
-	//default value
-	attr.label ??= "Close";
-	attr.type ??= "button";
-
-	//add btn-close class
-	//white
-	attr.class = mergeClass(attr.class, ["btn-close", attr.white ? "btn-close-white" : undefined]);
-
-	delete attr.white;
-
-	return attr;
-};
-
-export class btnclose extends button {
-	constructor();
-	constructor(attr: Btnclose);
-	constructor(...arg: any[]) {
-		super(bsConsNoElemArg(convert, arg));
-	}
 }
