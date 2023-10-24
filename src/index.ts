40ad27f483b920a0164ea563d9ba87742146e74b
printf83 <printf83@gmail.com>
2023-10-24 22:48:39 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import * as I from "./interface/index.js";
 import { tag as t } from "./core/tag.js";
 import { html as s } from "./core/html.js";
 
-export { core, t, s, h, b, I };
+export { core, core as c, t, s, h, b, I };
 
 //test if ts-reset is installed
 // const result = JSON.parse("{}");
--- src/interface/index.ts
+++ src/interface/index.ts
@@ -2,4 +2,4 @@ import * as h from "./html/_index.js";
 import * as b from "./bootstrap/_index.js";
 import * as c from "./core/_index.js";
 
-export { h, b, c };
+export { h, b, c, c as core };
